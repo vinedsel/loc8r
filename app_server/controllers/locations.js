f42b9f61f3ee3da0e7fb1a43a81491a09df902ae
Raymond Gregory <vinedsel@gmail.com>
2019-06-14 22:23:30 -0500

    Cleaned up code

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 // Get 'Home' Page
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', { 
-      title: 'Loc8r - find a place to work with wifi', 
+    res.render('locations-list', { title: 'Loc8r - find a place to work with wifi', 
       pageHeader: {
         title: 'Loc8r',
         strapline: 'Find places to work with wifi near you!'
       },
+      sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about.Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
       locations: [{
         name: 'Starcups',
         address: '125 High Street, Reading, RG6 1PS',
@@ -27,8 +27,6 @@ module.exports.homelist = function(req, res) {
       }]
     });
   };
-
-  
 
 // Get 'Location Info' Page
 module.exports.locationInfo = function(req, res) {
