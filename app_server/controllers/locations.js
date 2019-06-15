1cf3613370f6bbadbeb6adf5c74c4cedbfece82f
Raymond Gregory <vinedsel@gmail.com>
2019-06-14 19:59:03 -0500

    Replaced hard data with interpolated and buffered data systems, added inline JS for ratings system

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,34 @@
 // Get 'Home' Page
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', {title: 'Home'});
-};
+    res.render('locations-list', { 
+      title: 'Loc8r - find a place to work with wifi', 
+      pageHeader: {
+        title: 'Loc8r',
+        strapline: 'Find places to work with wifi near you!'
+      },
+      locations: [{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+        }, {
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+        }, {
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '250m'
+      }]
+    });
+  };
+
+  
 
 // Get 'Location Info' Page
 module.exports.locationInfo = function(req, res) {
