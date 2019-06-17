a30a089e521388ee4baf4fba10862c3c2b06db04
Raymond Gregory <vinedsel@gmail.com>
2019-06-16 19:52:23 -0500

    Added mongoose connection functions

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,13 @@
 var mongoose = require ( 'mongoose' );
+var dbURI = 'mongod://localhost/loc8r';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
